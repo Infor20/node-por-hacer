2af274ae64c2f9f432a84539f587fcb5c5270412
Angel <a.ramirez88@hotmail.com>
2020-03-05 16:02:20 -0300

    Primer commit

--- app.js
+++ app.js
@@ -11,25 +11,21 @@ switch (comando) {
         break;
     case 'listar':
         let listado = porHacer.getListado();
-
         for (let tarea of listado) {
             console.log('====Por Hacer====='.green);
             console.log(tarea.descripcion);
             console.log('Estado: ', tarea.completado);
             console.log('=================='.green);
         }
-
         break;
     case 'actualizar':
         let actualizado = porHacer.actualizar(argv.descripcion, argv.completado);
         console.log(actualizado);
         break;
-
     case 'borrar':
         let borrar = porHacer.borrar(argv.descripcion);
         console.log(borrar);
         break;
-
     default:
         console.log('No se encuentra este comando');
 }
